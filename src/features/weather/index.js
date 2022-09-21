2b3877c0b22d6ffdffb1329998458b022870dc5f
Nazar Bychyk <nazar1bychyk@gmail.com>
2022-09-22 00:28:40 +0200

    added getLocation component to weather and finished weather component

--- src/features/weather/index.js
+++ src/features/weather/index.js
@@ -1,12 +1,29 @@
-import useGetWeather from "./useGetWeather"
-import { Content, Ico, Element } from "./styled"
+import useGetWeather from "./useGetWeather";
+import { Content, Ico, Element } from "./styled";
 
 const Weather = () => {
-    const { data } = useGetWeather();
+    const { data, status } = useGetWeather();
+    switch (status) {
+        case "ok":
+            return (
+                <Content>
+                    <Ico src={require(`./ico/${data.weather.icon}.png`)} alt="" />
+                    <Element>{`${data.temp}°C`}</Element>
+                    <Element>{data.city_name}</Element>
+                </Content>
+            )
+        case "error":
+            return (
+                <Content>Something wrong</Content>
+            )
+        case "loading": return (
+            <Content>Loading...</Content>
+        )
+        default: return (
+            <Content>Waiting for GPS</Content>
+        )
+    }
 
-    return (
-        <Content><Ico src={require(`./ico/a01d.png`)} alt="" /> <Element>{`${data.temp}°C`}</Element> <Element>{data.city_name}</Element></Content>
-    )
 };
 
-export default Weather
+export default Weather;
--- src/features/weather/useGetWeather.js
+++ src/features/weather/useGetWeather.js
@@ -2,25 +2,57 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 const useGetWeather = () => {
-
-    const [data, setData] = useState("")
+    const [status, setStatus] = useState("loading");
+    const [data, setData] = useState();
 
     useEffect(() => {
-        const options = {
-            method: 'GET',
-            url: 'weather.json',
-            //  url: 'https://api.weatherbit.io/v2.0/current',
-            // params: { lon: '38.5', lat: '-78.5', key:"ba22399fefba4a1294cc2b57a1867d4e" },
+        const getWeather = (longitude, latitude) => {
+            const options = {
+                method: 'GET',
+                url: 'https://api.weatherbit.io/v2.0/current',
+                params: {
+                    lon: `${longitude}`,
+                    lat: `${latitude}`,
+                    key: "1b4de929b06f4e25afc77d5b867cd239",
+                },
+            };
+
+            axios.request(options).then(function (response) {
+                setData(response.data.data[0])
+                setStatus("ok")
+            }).catch(function (error) {
+                console.error(error);
+                setStatus("error")
+            });
         };
 
-        axios.request(options).then(function (response) {
-            setData(response.data.data[0])
-        }).catch(function (error) {
-            console.error(error);
-        });
+        const getCoords = (position) => {
+            try {
+                let latitude = position.coords.latitude.toFixed(2)
+                let longitude = position.coords.longitude.toFixed(2)
+                getWeather(longitude, latitude)
+            } catch (error) {
+                console.error(error)
+                setStatus("error")
+            }
+        };
+
+        const getLocation = () => {
+            try {
+                navigator.geolocation.getCurrentPosition(getCoords, setStatus("error"))
+                setStatus("")
+            }
+            catch (error) {
+                setStatus("error")
+                console.error(error)
+            }
+        };
+
+        setTimeout(() => getLocation(),500)
+
     }, []);
 
-    return { data }
+    return { data, status };
 };
 
-export default useGetWeather
+export default useGetWeather;
