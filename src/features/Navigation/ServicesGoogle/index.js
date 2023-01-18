341711e37f5a0682c9bca953be8a57b33247459b
Nazar Bychyk <nazar1bychyk@gmail.com>
2023-01-18 17:32:59 +0100

    Fixed showServicesBar in servicesGoogle

--- src/features/Navigation/ServicesGoogle/index.js
+++ src/features/Navigation/ServicesGoogle/index.js
@@ -1,54 +1,55 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Content, Button, Img, Block, Footer } from "./styled";
 import { ReactComponent as IconMenu } from './icons/bar.svg';
 
 const ServicesGoogle = () => {
-    let [showServicesBar, setShowServicesBar] = useState(false);
+    const [showServicesBar, setShowServicesBar] = useState(false);
 
     return (
         <>
-            <Button bar={showServicesBar} onClick={() => setShowServicesBar(showServicesBar = !showServicesBar)}><IconMenu /></Button>
-            <Content bar={showServicesBar}>
-                <Block href="https://mail.google.com/mail/?tab=rm">
-                    <Img src={require("./icons/gmail.png")} alt=""></Img>
-                    Gmail
-                </Block>
-                <Block href="https://docs.google.com/spreadsheets/?usp=sheets_alc&authuser=0">
-                    <Img src={require("./icons/exel.png")} alt=""></Img>
-                    Exel
-                </Block>
-                <Block href="https://docs.google.com/document/?usp=docs_alc&authuser=0">
-                    <Img src={require("./icons/word.png")} alt=""></Img>
-                    Word
-                </Block>
-                <Block href="https://maps.google.pl/maps?hl=pl&tab=rl">
-                    <Img src={require("./icons/maps.png")} alt=""></Img>
-                    Maps
-                </Block>
-                <Block href="https://www.youtube.com/?tab=r1">
-                    <Img src={require("./icons/youtube.png")} alt=""></Img>
-                    Youtube
-                </Block>
-                <Block href="https://keep.google.com/u/0">
-                    <Img src={require("./icons/keep.png")} alt=""></Img>
-                    Keep
-                </Block>
-                <Block href="https://translate.google.pl/?hl=pl&tab=rT">
-                    <Img src={require("./icons/translator.png")} alt=""></Img>
-                    Translator
-                </Block>
-                <Block href="https://drive.google.com/?tab=ro&authuser=0">
-                    <Img src={require("./icons/drive.png")} alt=""></Img>
-                    Drive
-                </Block>
-                <Block href="https://photos.google.com/?tab=rq&pageId=none">
-                    <Img src={require("./icons/images.png")} alt=""></Img>
-                    Images
-                </Block>
-                <Footer href="https://www.google.pl/intl/pl/about/products?tab=rh">More services</Footer>
-            </Content>
+            <Button bar={showServicesBar} onClick={() => setShowServicesBar(!showServicesBar)}><IconMenu /></Button>
+            {showServicesBar &&
+                <Content>
+                    <Block href="https://mail.google.com/mail/?tab=rm">
+                        <Img src={require("./icons/gmail.png")} alt=""></Img>
+                        Gmail
+                    </Block>
+                    <Block href="https://docs.google.com/spreadsheets/?usp=sheets_alc&authuser=0">
+                        <Img src={require("./icons/exel.png")} alt=""></Img>
+                        Excel
+                    </Block>
+                    <Block href="https://docs.google.com/document/?usp=docs_alc&authuser=0">
+                        <Img src={require("./icons/word.png")} alt=""></Img>
+                        Word
+                    </Block>
+                    <Block href="https://maps.google.pl/maps?hl=pl&tab=rl">
+                        <Img src={require("./icons/maps.png")} alt=""></Img>
+                        Maps
+                    </Block>
+                    <Block href="https://www.youtube.com/?tab=r1">
+                        <Img src={require("./icons/youtube.png")} alt=""></Img>
+                        Youtube
+                    </Block>
+                    <Block href="https://keep.google.com/u/0">
+                        <Img src={require("./icons/keep.png")} alt=""></Img>
+                        Keep
+                    </Block>
+                    <Block href="https://translate.google.pl/?hl=pl&tab=rT">
+                        <Img src={require("./icons/translator.png")} alt=""></Img>
+                        Translator
+                    </Block>
+                    <Block href="https://drive.google.com/?tab=ro&authuser=0">
+                        <Img src={require("./icons/drive.png")} alt=""></Img>
+                        Drive
+                    </Block>
+                    <Block href="https://photos.google.com/?tab=rq&pageId=none">
+                        <Img src={require("./icons/images.png")} alt=""></Img>
+                        Images
+                    </Block>
+                    <Footer href="https://www.google.pl/intl/pl/about/products?tab=rh">More services</Footer>
+                </Content>}
         </>
-    )
+    );
 };
 
 export default ServicesGoogle;
--- src/features/Navigation/ServicesGoogle/styled.js
+++ src/features/Navigation/ServicesGoogle/styled.js
@@ -1,66 +1,63 @@
 import styled, { css } from "styled-components";
 
 export const Button = styled.div`
-    display:flex;
-    justify-content:center;
-    align-items:center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     cursor: pointer;
-    margin:20px;
-    width:40px;
-    height:40px;
-    border-radius:50%;
+    margin: 20px;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
     :hover{
-        background-color:${({ theme }) => theme.color.hover};
+        background-color: ${({ theme }) => theme.color.hover};
     }
     ${({ bar }) => bar && css`
-        background-color:${({ theme }) => theme.color.hover};
+        background-color: ${({ theme }) => theme.color.hover};
     `}
-`
+`;
 
 export const Content = styled.div`
-    text-align:center;
+    display: grid;
+    text-align: center;
     position: absolute;
-    grid-template-columns:repeat(3, 100px);
+    grid-template-columns: repeat(3, 100px);
     grid-template-rows: repeat(4, 1fr);
-    display:none;
-    padding:10px;
+    padding: 10px;
     top: 0;
     right: 0;
-    background-color:${({ theme }) => theme.color.background};
-    margin:60px;
-    width:300px;
-    height:350px;
-    border-radius:10px;
-    ${({ bar }) => bar && css`
-         display:grid;
-    `}
-`
+    background-color: ${({ theme }) => theme.color.background};
+    margin: 60px;
+    width: 300px;
+    height: 350px;
+    border-radius: 10px;
+`;
 
 export const Block = styled.a`
-    max-height:50px;
-    padding:20px;
-    margin:10px;
-    display:flex;
-    flex-direction:column;
-    text-decoration:none;
-    align-items:center;
-    border-radius:10px;
-    color:${({ theme }) => theme.color.text};
+    max-height: 50px;
+    padding: 20px;
+    margin: 10px;
+    display: flex;
+    flex-direction: column;
+    text-decoration: none;
+    align-items: center;
+    border-radius: 10px;
+    color: ${({ theme }) => theme.color.text};
     cursor: pointer;
     :hover{
         background-color: ${({ theme }) => theme.color.hover};
     }
-`
+`;
 
 export const Footer = styled.a`
-    color:${({ theme }) => theme.color.text};
+    color: ${({ theme }) => theme.color.text};
     grid-column-start: 2;
-    text-decoration:none;
+    text-decoration: none;
     :hover{
         border-bottom: 1px solid ${({ theme }) => theme.color.text};
     }
-`
+`;
 
 export const Img = styled.img`
-    height:40px;
-`
+    height: 40px;
+`;
