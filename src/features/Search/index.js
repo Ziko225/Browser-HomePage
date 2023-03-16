a1bfa5adc32c16d725404a100c71b02b5cfa65b3
Nazar Bychyk <nazar1bychyk@gmail.com>
2023-03-16 11:06:56 +0100

    Changed protocol to https in seacrh component

--- src/features/Search/index.js
+++ src/features/Search/index.js
@@ -3,7 +3,7 @@ import { Content, InputBox, StyledInput, StyledSearchIco } from "./styled";
 const Search = () => {
     return (
         <Content>
-            <form action="http://www.google.com/search" method="get">
+            <form action="https://www.google.com/search" method="get">
                 <InputBox><StyledSearchIco /><StyledInput placeholder="Search with google" type="search" name="q" /></InputBox>
             </form>
         </Content>
